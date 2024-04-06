25ced931e368b8959753cbc940e522fbab9ea073
Ashish Agarwal <code.with.aasheesh@gmail.com>
2024-04-06 21:58:56 +0530

    a

--- my_flask_server.py
+++ my_flask_server.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+name = "Ashish Agarwal"
+
 @app.route('/')
 def hello():
-    return render_template("index.html")
+    return render_template("index.jinja", name=name)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080, host='0.0.0.0')
