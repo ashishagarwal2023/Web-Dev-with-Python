a7e65a08a3b85a10c3c05f118e99f075d9f473f9
Ashish Agarwal <code.with.aasheesh@gmail.com>
2024-04-07 17:42:46 +0530

    a

--- portfolio/app.py
+++ portfolio/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, world!"
+    return render_template("index.jinja")
+
+@app.route("/about")
+def aboutMe():
+    return render_template("about.jinja")
 
 if __name__ == "__main__":
     app.run(debug=True)
